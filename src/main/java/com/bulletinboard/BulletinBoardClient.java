82ac630e1473018995660111121376c4f144dfe6
Arth Desai <33043106+asd150@users.noreply.github.com>
2018-10-12 22:35:48 -0400

    100%

--- src/main/java/com/bulletinboard/BulletinBoardClient.java
+++ src/main/java/com/bulletinboard/BulletinBoardClient.java
@@ -6,10 +6,7 @@ import io.grpc.ServerBuilder;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,6 +19,7 @@ public class BulletinBoardClient{
 
         BulletinBoardSGrpc.BulletinBoardSBlockingStub stub = BulletinBoardSGrpc.newBlockingStub(channel);
 
+       // stub.post(msg.newBuilder().setTitle("Hello").setBody("This is first message").build());
 
 //    Iterator<confirm> it =  stub.post(com.bulletinboard.msg.newBuilder().setTitle("Title").setBody("This is body").build());
 //       int i=1;
@@ -30,6 +28,39 @@ public class BulletinBoardClient{
 //            i++;
 //        }
 //	}
+
+        //lets post some notice
+        //stub.post(msg.newBuilder().setTitle("1").setTitle("Title 1").build());
+
+        while(true) {
+            System.out.println("Input a Command");
+            Scanner sn = new Scanner(System.in);
+            String str = sn.nextLine();
+
+            System.out.println();
+            String[] cmd = str.split("\\s+");
+
+            if (cmd[0].equalsIgnoreCase("post")) {
+                confirm response = stub.post(msg.newBuilder().setTitle(cmd[1]).setBody(cmd[2]).build());
+                System.out.println(response);
+            } else if (cmd[0].equalsIgnoreCase("get")) {
+                msg msg = stub.get(confirm.newBuilder().setConfirmation(cmd[1]).build());
+                System.out.println(msg.getTitle() + " " + msg.getBody());
+            } else if (cmd[0].equalsIgnoreCase("delete")) {
+                confirm confirm = stub.delete(com.bulletinboard.confirm.newBuilder().setConfirmation(cmd[1]).build());
+                System.out.println(confirm.getConfirmation());
+
+            } else if (cmd[0].equalsIgnoreCase("list")) {
+                Iterator<msg> it = stub.list(confirm.newBuilder().setConfirmation("getList").build());
+                //System.out.println(it.hasNext());
+                while(it.hasNext()){
+                   msg msg = it.next();
+                    System.out.println(msg.getTitle() + "  " + msg.getBody());
+                }
+
+
+            }
+        }
 
 
     }
--- src/main/java/com/bulletinboard/BulletinBoardService.java
+++ src/main/java/com/bulletinboard/BulletinBoardService.java
@@ -11,6 +11,22 @@ class object{
         this.title = title;
         this.body = body;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
 public class BulletinBoardService extends BulletinBoardSGrpc.BulletinBoardSImplBase {
     //@Override
@@ -33,16 +49,46 @@ public class BulletinBoardService extends BulletinBoardSGrpc.BulletinBoardSImplB
 
     @Override
     public void get(confirm request, StreamObserver<msg> responseObserver) {
-        super.get(request, responseObserver);
+       //given a title ... return the body
+        String titleGiven  = request.getConfirmation();
+        int indexFound = 0;
+        for(int i=0;i<lists.size();i++){
+            if(lists.get(i).getTitle().equals(titleGiven)){
+                indexFound = i;
+                break;
+            }
+        }
+        responseObserver.onNext(msg.newBuilder().setTitle(lists.get(indexFound).getTitle()).setBody(lists.get(indexFound).getBody()).build());
+        responseObserver.onCompleted();
+
     }
 
     @Override
     public void list(confirm request, StreamObserver<msg> responseObserver) {
-        super.list(request, responseObserver);
+        System.out.println(lists.size() + " list");
+        if(lists.size()==0){
+            responseObserver.onNext(msg.newBuilder().setTitle("No Posts").setTitle("Please post on the board").build());
+            responseObserver.onCompleted();
+        }
+        else{
+            System.out.println("LOOP1 ");
+            for(int i=0;i<lists.size();i++){
+                System.out.println("LOOP2 ");
+                responseObserver.onNext(msg.newBuilder().setTitle(lists.get(i).getTitle()).setBody(lists.get(i).getBody()).build());
+            }
+            responseObserver.onCompleted();
+        }
     }
 
     @Override
     public void delete(confirm request, StreamObserver<confirm> responseObserver) {
-        super.delete(request, responseObserver);
+        String toDelete = request.getConfirmation();
+        for(int i=0;i<lists.size();i++){
+            if(lists.get(i).getTitle().equals(toDelete)){
+                lists.remove(i);
+            }
+        }
+        responseObserver.onNext(confirm.newBuilder().setConfirmation("Deleted " + toDelete).build());
+        responseObserver.onCompleted();
     }
 }
